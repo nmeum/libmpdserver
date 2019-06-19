00eae0d29c5bb55fcda8b52368d763baab0af0f6
Sören Tempel <soeren+git@soeren-tempel.net>
2019-06-19 20:43:07 +0200

    queue: add support for moveid command

--- queue.c
+++ queue.c
@@ -19,8 +19,23 @@ mpd_delete(void)
 	               mpd_argument(mpd_range()), free);
 }
 
+mpdf_fold(moveid, MPD_ARG_UINT MPD_ARG_INT)
+
+static mpc_parser_t *
+mpd_moveid(void)
+{
+	mpc_parser_t *toval, *to;
+
+	toval = mpc_or(2, mpc_and(2, mpcf_strfold, mpc_char('-'), mpc_digits()),
+	               mpc_digits());
+	to = mpc_expect(mpc_apply(toval, mpcf_int), "signed integer");
+
+	return mpc_and(3, mpdf_moveid, mpc_string("moveid"),
+	               mpd_argument(mpd_uint()), mpd_argument(to), free, free);
+}
+
 mpc_parser_t *
 mpd_queue_cmds(void)
 {
-	return mpc_or(2, mpd_add(), mpd_delete());
+	return mpc_or(3, mpd_add(), mpd_delete(), mpd_moveid());
 }
