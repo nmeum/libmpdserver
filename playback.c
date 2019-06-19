89ade4399dbe2d469366566cb9cfc508397ac28d
Sören Tempel <soeren+git@soeren-tempel.net>
2019-06-19 20:31:47 +0200

    playback: add support for the setvol command

--- playback.c
+++ playback.c
@@ -1,6 +1,15 @@
 #include "fns.h"
 #include "mpc.h"
 
+static int
+mpd_check_volume(mpc_val_t **val)
+{
+	unsigned int *ptr;
+
+	ptr = *(unsigned **)val;
+	return *ptr <= 100;
+}
+
 mpdf_fold(consume, MPD_ARG_UINT)
 
 static mpc_parser_t *
@@ -19,8 +28,20 @@ mpd_crossfade(void)
 	               mpd_argument(mpd_uint()), free);
 }
 
+mpdf_fold(setvol, MPD_ARG_UINT)
+
+static mpc_parser_t *
+mpd_setvol(void)
+{
+	mpc_parser_t *vol;
+
+	vol = mpc_check(mpd_uint(), free, mpd_check_volume, "invalid volume");
+	return mpc_and(2, mpdf_setvol, mpc_string("setvol"), mpd_argument(vol),
+	               free);
+}
+
 mpc_parser_t *
 mpd_playback_cmds(void)
 {
-	return mpc_or(2, mpd_consume(), mpd_crossfade());
+	return mpc_or(3, mpd_consume(), mpd_crossfade(), mpd_setvol());
 }
