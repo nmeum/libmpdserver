1d247aa93c11104dcf2d74df351e2d7bdc5f4bd2
Sören Tempel <soeren+git@soeren-tempel.net>
2019-07-09 19:37:07 +0200

    control: add support for pause

--- control.c
+++ control.c
@@ -7,6 +7,16 @@ mpd_next(void)
 	return mpd_cmd_noarg("next");
 }
 
+mpdf_fold(pause, mpd_opt_arg(MPD_ARG_BOOL))
+
+static mpc_parser_t *
+mpd_pause(void)
+{
+	/* TODO: usage without argument is deprecated */
+	return mpc_and(2, mpdf_pause, mpc_string("pause"),
+	               mpc_maybe(mpd_argument(mpd_binary())), free);
+}
+
 mpdf_fold(seek, MPD_ARG_UINT MPD_ARG_FLOAT)
 
 static mpc_parser_t *
@@ -20,5 +30,5 @@ mpd_seek(void)
 mpc_parser_t *
 mpd_control_cmds(void)
 {
-	return mpc_or(2, mpd_next(), mpd_seek());
+	return mpc_or(3, mpd_next(), mpd_pause(), mpd_seek());
 }
